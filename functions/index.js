ff9921c731e029f0fc3dd21960cbd96ec10d9009
sjzenith <sjzenith0723@gmail.com>
2020-06-18 17:53:43 +0900

    fitth

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,21 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-//http request 1
-exports.randomNumber = functions.https.onRequest((request,response)=>{
-  const number = Math.round(Math.random() * 100);
-  response.send(number.toString());
+//auth trigger (new user sign up)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+  console.log('user created', user.email, user.uid);
+  //promise또는 value를 리턴해줘야 한다
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: []
+  });
 });
 
-// http request 2
-exports.toTheDojo = functions.https.onRequest((request, response) => {
-  response.redirect('https://www.thenetninja.co.uk');
-});
+//auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  console.log('user deleted',user.email, user.uid);
 
-// http callable function
-exports.sayHello = functions.https.onCall((data, context) => {
-  const name = data.name;
-  return `hello ${name} :)`;
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
 });
